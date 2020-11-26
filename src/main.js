a585a1917ef1c69bd73c8c78667fa2267c37f05d
Romain <romainfpro@gmail.com>
2020-11-26 11:33:28 +0100

    Add plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router'; 
+import VueRouter from 'vue-router';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
