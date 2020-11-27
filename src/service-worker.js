a01592d902ed8c9f4d0eaea12df11558507edf82
Romain <romainfpro@gmail.com>
2020-11-27 15:14:40 +0100

    SW test 11

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,13 +1,13 @@
 workbox.routing.registerRoute(
     ({url}) => url.pathname.endsWith('jpg'),
-        new workbox.strategies.NetworkFirst({
+        new workbox.strategies.CacheFirst({
             cacheName: "imageCache",
         })
 )
 
 workbox.routing.registerRoute(
     ({url}) => url.pathname.endsWith('mp3'),
-        new workbox.strategies.NetworkFirst({
+        new workbox.strategies.CacheFirst({
             cacheName: "mp3Cache",
         })
 )
